409a96239583447f41e6adc17787055201da6b4d
Henry Rivera <henryarturorivera@gmail.com>
2022-09-17 22:06:00 +0200

    fix errors in build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBjcdtVH_1WSpjcPfE8wax_yqLLOREr0Vw",
+    authDomain: "firststore-49d49.firebaseapp.com",
+    projectId: "firststore-49d49",
+    storageBucket: "firststore-49d49.appspot.com",
+    messagingSenderId: "118405926880",
+    appId: "1:118405926880:web:dd0265a24650911aa92585"
+  }
 };
